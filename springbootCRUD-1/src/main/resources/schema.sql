89706962fc27379926cd5542af62044354cd7152
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-12-11 15:54:50 +0530

    Add files via upload

---
+++ springbootCRUD-1/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS studentdata (
+    id INT PRIMARY KEY ,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    mobile VARCHAR(20) NOT NULL
+);
